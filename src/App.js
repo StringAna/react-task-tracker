89469e6532e7069db6fb616e5383791e6dfbfb68
StringAna <tewaryantara21@gmail.com>
2021-08-03 09:25:40 +0530

    Proptypes imported to Header.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="Container">
-      <Header />
+      <Header title={12} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Header = ({ title }) => {
   return (
@@ -10,6 +11,10 @@ const Header = ({ title }) => {
 
 Header.defaultProps = {
   title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
 };
 
 export default Header;
