81ed628a63fedc428faf69064d848c1428db7a6b
StringAna <tewaryantara21@gmail.com>
2021-08-12 20:04:34 +0530

    Setting tasks as global state in App.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,4 @@
-import { useState } from "react";
-
-const Tasks = () => {
-  const [tasks, settasks] = useState([
-    {
-      id: "1",
-      text: "Doc appointment",
-      day: "Feb 5th at 2:30 pm",
-      reminder: true,
-    },
-    {
-      id: "2",
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true,
-    },
-    {
-      id: "3",
-      text: "Food shopping",
-      day: "Feb 5th at 2:30pm",
-      reminder: false,
-    },
-  ]);
+const Tasks = ({ tasks }) => {
   return (
     <div>
       {/*Map through tasks, and output something based on each tasks element(create List) 
